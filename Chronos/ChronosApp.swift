3fd64d087d9067459bed3117ba63f61980789a74
Jakob Nordhagen <jakobn@stanford.edu>
2021-03-15 14:29:15 -0700

    Initial Commit

---
+++ Chronos/ChronosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronosApp.swift
+//  Chronos
+//
+//  Created by Jakob Nordhagen on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ChronosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
